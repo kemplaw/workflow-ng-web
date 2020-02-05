3f9c318169d3ed538205d48e321d2cf56876bcac
kemplaw <kemplaw@hotmail.com>
2020-02-05 12:18:23 +0800

    @update: 完成任务添加功能。

--- src/app/shared/domain/task.ts
+++ src/app/shared/domain/task.ts
@@ -3,9 +3,10 @@ export interface Task {
   name: string // 任务名称
   completed: boolean // 任务是否完成
   priority: string // 任务优先级
-  principal: string // 任务负责人
-  startTime: string // 任务开始时间
-  endTime: string // 任务结束时间
+  principal?: string // 任务负责人
+  startTime?: string // 任务开始时间
+  endTime?: string // 任务结束时间
+  remarks?: string // 备注
 }
 
 export interface TaskStatus {
--- src/app/tasks/components/task-container/task-container.component.ts
+++ src/app/tasks/components/task-container/task-container.component.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core'
 import { Tab } from 'src/app/shared/components'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
-import { TaskStatus } from 'src/app/shared'
+import { TaskStatus, Task } from 'src/app/shared'
 
 @Component({
   selector: 'app-task-container',
@@ -41,6 +41,9 @@ export class TaskContainerComponent implements OnInit {
   ]
   activedTab: Tab = { label: '任务', value: '1' }
 
+  private _currentTaskStatus: TaskStatus // 当前的任务状态数据
+  private _currentTask: Task // 当前选中的任务数据
+
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
@@ -53,12 +56,36 @@ export class TaskContainerComponent implements OnInit {
     })
   }
 
-  handleCreateTask() {
+  handleCreateTask(taskStatus: TaskStatus) {
     this.taskDialogVisible = 'show'
+    this._currentTaskStatus = taskStatus
   }
 
-  handleTaskDialogClose() {
-    console.log(this.taskDialogForm)
+  // 点击确认
+  handleConfirmTaskForm() {
+    const { id } = this._currentTaskStatus
+    const {
+      title,
+      date,
+      remarks,
+      priority,
+      principal
+    } = this.taskDialogForm.value
+
+    const newTaskData: Task = {
+      id: '1',
+      name: title,
+      startTime: date.start,
+      endTime: date.end,
+      remarks,
+      priority,
+      principal,
+      completed: false
+    }
+
+    this.taskStatusList
+      .find(taskStatus => taskStatus.id === id)
+      .Tasks.push(newTaskData)
   }
 
   // 点击添加任务状态按钮
